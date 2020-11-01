284dc8a96442bd72282400733a586d52f4accb64
abhishekbreddy <43873538+abhishekbreddy@users.noreply.github.com>
2020-11-01 13:01:48 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Search from '/home/abhi/Desktop/capstone1/job/src/Components/Search.js'
+import Joblist from '/home/abhi/Desktop/capstone1/job/src/Components/joblist.js'
+
+
+	export default class home extends React.Component{
+        render(){
+            return(<div>
+                
+                <Joblist/>
+                </div>
+            )
+        }
+    }	
+		
