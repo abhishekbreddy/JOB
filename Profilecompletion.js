cd18e5192ec68b86814476bb3eade5e76a5796ef
abhishekbreddy <43873538+abhishekbreddy@users.noreply.github.com>
2020-11-01 13:03:05 +0530

    Add files via upload

---
+++ Profilecompletion.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Profile extends React.Component{
+    render(){
+        return(<div><label>
+            name:
+            <input />
+            school:
+            <input />
+            college
+            </label>
+            
+            <input></input>
+
+        </div>)
+    }
+}
---
+++ joblist.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+import Search from '/home/abhi/Desktop/capstone1/job/src/Components/Search.js'
+import Results from '/home/abhi/Desktop/capstone1/job/src/Components/Results.js'
+import Popup from '/home/abhi/Desktop/capstone1/job/src/Components/Popup.js'
+
+function Joblist() {
+  const [state, setState] = useState({    // we destructured it
+    s: "",
+    results: [],
+    selected: {}
+  });
+  const apiurl = "http://www.omdbapi.com/?apikey=dfe6d885";
+
+  const search = (e) => {
+    if (e.key === "Enter") {
+      axios(apiurl + "&s=" + state.s).then(({ data }) => {
+        let results = data.Search;
+
+        setState(prevState => {
+          return { ...prevState, results: results }
+        })
+      });
+    }
+  }
+  
+  const handleInput = (e) => {
+    let s = e.target.value;
+
+    setState(prevState => {
+      return { ...prevState, s: s }
+    });
+  }
+
+  const openPopup = id => {
+    axios(apiurl + "&i=" + id).then(({ data }) => {
+      let result = data;
+
+      console.log(result);
+
+      setState(prevState => {
+        return { ...prevState, selected: result }
+      });
+    });
+  }
+
+  const closePopup = () => {
+    setState(prevState => {
+      return { ...prevState, selected: {} }
+    });
+  }
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Search for a movie</h1>
+      </header>
+      <main>
+        <Search handleInput={handleInput} search={search} />
+
+        <Results results={state.results} openPopup={openPopup} />
+
+        {(typeof state.selected.Title != "undefined") ? <Popup selected={state.selected} closePopup={closePopup} /> : false}
+      </main>
+    </div>
+  );
+}
+
+export default Joblist
