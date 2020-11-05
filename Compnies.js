7e13852495a7646fd2f559c4418bf93b1a6e5c82
abhishekbreddy <43873538+abhishekbreddy@users.noreply.github.com>
2020-11-05 19:13:14 +0530

    Add files via upload

--- Compnies.js
+++ Compnies.js
@@ -1,6 +1,33 @@
-import React from 'react'
-export default class Companies extends React.Component{
-    render(){
-        return(<h1>company</h1>);
-    }
-}
+import React, { useState ,useEffect} from 'react'
+import Getdata from '/home/abhi/Desktop/capstone1/job/src/Components/Servercall.js'
+
+function Companies(){
+   useEffect(()=>{
+ Getdata().then((res)=>{
+     //alert('message')
+     setinfo(res.data)
+ }
+
+
+    )
+ 
+ 
+   },[])
+   
+   
+    const [info,setinfo]=useState([])
+    return(
+        <div>
+            <table className="table table-bordered"style={{backgroundColor:'lightblue'}} ><tr><th>id</th><th>username</th></tr>
+            {
+                info.map((obj,index)=>{
+                return(<tr><td>{obj.id}</td>
+                <td>{obj.username}</td></tr>)
+                })
+            }
+                
+            </table>
+    </div>
+
+     ) }
+        export default Companies
