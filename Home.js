b2f226db0b7b87c97bf4fc93430b787dac6f1811
abhishekbreddy <43873538+abhishekbreddy@users.noreply.github.com>
2020-11-05 19:08:19 +0530

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import Search from '/home/abhi/Desktop/capstone1/job/src/Components/Search.js'
 import Joblist from '/home/abhi/Desktop/capstone1/job/src/Components/joblist.js'
-
+import Apply from '/home/abhi/Desktop/capstone1/job/src/Components/Apply.js'
 
 	export default class home extends React.Component{
         render(){
             return(<div>
                 
                 <Joblist/>
+                <Apply/>
                 </div>
             )
         }
-    }	
+    }
 		
